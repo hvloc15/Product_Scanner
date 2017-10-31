b1529ca9d0acf1dff5ca438e03fa2e07f6e4f3ae
Terrorists <hvloc15@apcs.vn>
2017-10-31 23:01:41 +0700

    add toast

--- src/app/src/main/java/terrorist/product_scanner_project/MainActivity.java
+++ src/app/src/main/java/terrorist/product_scanner_project/MainActivity.java
@@ -1,14 +1,15 @@
 package terrorist.product_scanner_project;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView hk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Loc, World Class Developer",Toast.LENGTH_LONG).show();
     }
 }
