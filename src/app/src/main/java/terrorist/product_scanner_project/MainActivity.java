97c053d062359a12357c53426b3409757e6b03b7
huseyin39 <huseyin.kizil@ensta-bretagne.org>
2017-10-31 22:44:18 +0700

    hk2

--- src/app/src/main/java/terrorist/product_scanner_project/MainActivity.java
+++ src/app/src/main/java/terrorist/product_scanner_project/MainActivity.java
@@ -2,9 +2,10 @@ package terrorist.product_scanner_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView hk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
