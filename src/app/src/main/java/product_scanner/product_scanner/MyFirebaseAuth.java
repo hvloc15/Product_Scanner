c68fc10eaefae63984584b00c7dc958f3d95fb67
Terrorists <hvloc15@apcs.vn>
2017-11-25 09:10:59 +0700

    update source code

--- src/app/src/main/java/product_scanner/product_scanner/MyFirebaseAuth.java
+++ src/app/src/main/java/product_scanner/product_scanner/MyFirebaseAuth.java
@@ -2,7 +2,6 @@ package product_scanner.product_scanner;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * Created by Nguyen Khang on 11/16/2017.
@@ -11,5 +10,4 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MyFirebaseAuth {
     static public FirebaseAuth mAuth;
     static public FirebaseUser mUser;
-    FirebaseFirestore
 }
